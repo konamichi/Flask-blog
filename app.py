41f0444d155ac00aa020c39ef96cc999ae16032e
Julia <chistokletova20022@gmail.com>
2022-09-26 13:09:07 +0300

    feat: add user page

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ app = Flask(__name__)
 def index():
     return 'Hello!'
 
-@app.route('/about')
-def about():
-    return 'About this page'
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User page: ' + name + ' - ' + str(id)
+
 
 if __name__ == "__main__":
-    app.run(debug=True) # Видимость ошибок
+    app.run(debug=True)  # Видимость ошибок
