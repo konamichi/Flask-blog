4d4c3bec2e276fe5e561fb8e6ec753b9635b9afd
Julia <chistokletova20022@gmail.com>
2022-09-27 21:20:06 +0300

    fix: update app.py
    Delete user tracking page

--- app.py
+++ app.py
@@ -30,9 +30,6 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'User page: ' + name + ' - ' + str(id)
 
 
 if __name__ == "__main__":
